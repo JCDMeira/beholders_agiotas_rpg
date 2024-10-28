519d40948d5fb347410fe7b0623c3e72fae22596
Jean Meira <jean.meira10@hotmail.com>
2024-10-28 15:08:23 -0300

    feat: initial setup

---
+++ README.md
@@ -0,0 +1,21 @@
+# Projeto para guardar histórias dos personagens para o jogo de RPG BA&A
+
+# 📋 Índice
+
+- [Proposta](#id01)
+  - [Conclusões](#id01.01)
+- [Feito com](#id04)
+
+# 🚀 Proposta <a name="id01"></a>
+
+O objetivo desse projeto é manter um registro das histórias dos personagens do jogo de RPG BA&A. Isso ajudará a centralizar todos os personagens para consulta.
+
+# 🛠 Feito com <a name="id04"></a>
+
+<br />
+
+- astro
+- vs code
+- template [astrowind](https://github.com/onwidget/astrowind)
+
+<br />
---
+++ src/navigation.js
@@ -0,0 +1,41 @@
+import { getPermalink, getBlogPermalink } from './utils/permalinks';
+
+export const headerData = {
+  links: [
+    {
+      text: 'Homes',
+      href: '/',
+      // links: [
+      //   {
+      //     text: 'SaaS',
+      //     href: getPermalink('/homes/saas'),
+      //   },
+      //   {
+      //     text: 'Startup',
+      //     href: getPermalink('/homes/startup'),
+      //   },
+      //   {
+      //     text: 'Mobile App',
+      //     href: getPermalink('/homes/mobile-app'),
+      //   },
+      //   {
+      //     text: 'Personal',
+      //     href: getPermalink('/homes/personal'),
+      //   },
+      // ],
+    },
+    {
+      text: 'Sobre os Personagens',
+      links: [
+        {
+          text: 'Todos os personagens',
+          href: getBlogPermalink(),
+        },
+        {
+          text: 'Por player',
+          href: getPermalink('jean_meira', 'category'),
+        },
+      ],
+    },
+  ],
+};
